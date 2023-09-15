b92ca4dadf5e101c6f4fd523467aa590aac3a0bb
Abhishek Chaudhari <abhishek.chaudhari44@gmail.com>
2023-09-15 09:04:30 +0530

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Abhishek Chaudhari on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
